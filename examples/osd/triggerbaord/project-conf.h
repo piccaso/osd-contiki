1d2447d69da09cd169016aedf0f5195b67932ccf
Harald Pichler <harald@the-develop.net>
2016-08-18 17:30:39 +0200

    add noncorsec section

--- examples/osd/triggerbaord/project-conf.h
+++ examples/osd/triggerbaord/project-conf.h
@@ -82,4 +82,22 @@
 #define COAP_LINK_FORMAT_FILTERING      0
 */
 
+/*
+#undef LLSEC802154_CONF_ENABLED
+#define LLSEC802154_CONF_ENABLED          1
+#undef NETSTACK_CONF_FRAMER
+#define NETSTACK_CONF_FRAMER              noncoresec_framer
+#undef NETSTACK_CONF_LLSEC
+#define NETSTACK_CONF_LLSEC               noncoresec_driver
+#undef NONCORESEC_CONF_SEC_LVL
+#define NONCORESEC_CONF_SEC_LVL           1
+
+#define NONCORESEC_CONF_KEY { 0x00 , 0x01 , 0x02 , 0x03 , \
+                              0x04 , 0x05 , 0x06 , 0x07 , \
+                              0x08 , 0x09 , 0x0A , 0x0B , \
+                              0x0C , 0x0D , 0x0E , 0x0F }
+
+*/
+
 #endif /* PROJECT_RPL_WEB_CONF_H_ */
+
