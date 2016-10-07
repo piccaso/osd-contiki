e410b2ddd22c0bce6c2e4ee98e1db250410660b2
Harald Pichler <harald@the-develop.net>
2016-10-07 15:29:03 +0200

    add WCHaracter

--- cpu/avr/dev/arduino/Arduino.h
+++ cpu/avr/dev/arduino/Arduino.h
@@ -143,8 +143,8 @@ extern const uint8_t PROGMEM digital_pin_to_bit_mask_PGM[];
 
 #ifdef __cplusplus
 // look at this again when considering implementing serial
-//#include "WCharacter.h"
-//#include "WString.h"
+#include "WCharacter.h"
+#include "WString.h"
 //#include "HardwareSerial.h"
 
 uint16_t makeWord(uint16_t w);
