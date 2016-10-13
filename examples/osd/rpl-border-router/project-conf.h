a152a30958f2ed8d153ec730eb5b8e10baaafcf3
Harald Pichler <harald@the-develop.net>
2016-10-13 12:41:02 +0200

    add mac layer security

--- examples/osd/rpl-border-router/project-conf.h
+++ examples/osd/rpl-border-router/project-conf.h
@@ -62,4 +62,21 @@
 #define WEBSERVER_CONF_CFS_CONNS 2
 #endif
 
+/*
+#undef LLSEC802154_CONF_ENABLED
+#define LLSEC802154_CONF_ENABLED          1
+#undef NETSTACK_CONF_FRAMER
+#define NETSTACK_CONF_FRAMER              noncoresec_framer
+#undef NETSTACK_CONF_LLSEC
+#define NETSTACK_CONF_LLSEC               noncoresec_driver
+#undef NONCORESEC_CONF_SEC_LVL
+#define NONCORESEC_CONF_SEC_LVL           1
+
+#define NONCORESEC_CONF_KEY { 0x00 , 0x01 , 0x02 , 0x03 , \
+                              0x04 , 0x05 , 0x06 , 0x07 , \
+                              0x08 , 0x09 , 0x0A , 0x0B , \
+                              0x0C , 0x0D , 0x0E , 0x0F }
+
+*/
+
 #endif /* PROJECT_ROUTER_CONF_H_ */
