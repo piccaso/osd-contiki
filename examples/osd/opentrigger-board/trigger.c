f8a399d3466049859a4f6e7f297970aabb017859
Harald Pichler <harald@the-develop.net>
2016-12-04 15:47:40 +0100

    initial upload

---
+++ examples/osd/opentrigger-board/trigger.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "opentrigger.h"
+AUTOSTART_PROCESSES(&arduino_sketch);
+
+
+/*
+ * VI settings, see coding style
+ * ex:ts=8:et:sw=2
+ */
+
+/** @} */
